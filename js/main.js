1478314538e561dbc58889e9e0991c236a125be3
Yoandel Obispa <302631801@student.rocmondriaan.nl>
2022-10-26 14:07:04 +0200

    update

--- js/main.js
+++ js/main.js
@@ -7,88 +7,164 @@ let playerScore = 0;
 let computerScore = 0;
 let computerWins = 0;
 let playerWins = 0;
-let playerCredit = 5;
+// let playerCredit;
 let myMusic = document.getElementById("myAudio");
-myMusic.volume = 0.2;
+myMusic.volume = 0.050;
 
-const computerBtn = document.body.querySelector('.computer-button');
+let higher = document.querySelector('.higher');
+let lower = document.querySelector('.lower');
+let draw = document.querySelector('.draw');
+
+const computerBtn = document.body.querySelector('.computer-button')
 const playerBtn = document.body.querySelector('.player-button');
 const submit = document.body.querySelector('.submit');
-
-const playerName = prompt ("Wat is je username?. (Minstens 6 letters of nummers)", 'Sweaty');
-
-document.getElementById("demo").innerHTML = playerName;
-document.getElementById("demo1").innerHTML = playerName;
-
-
-function Agecheck () {
-if (ageRestriction > 0 && ageRestriction < 18) {
-  alert(`Je bent onder de 18. Jij bent ${ageRestriction} jaar oud.`);
-  window.location.href = "tooYoung.html";
-} else if (ageRestriction > "a" && ageRestriction < "z") {
-  window.location.href = "fout.html";
-  alert('Er is een fout opgetreden. Type alleen nummers (bijv. 18).');
-} else if (ageRestriction > "A" && ageRestriction < "Z") {
-  alert('Er is een fout opgetreden. Type alleen nummers (bijv. 18).');
-  window.location.href = "fout.html";
-} else{
-  alert(`Je bent ${ageRestriction} jaar oud.`);
-
-  startGame();
-  
-}}
-
+const playerName = prompt ("Wat is je username?.", 'Sweaty');
 const ageRestriction= prompt ("Hoe oud ben je?", 18);
 Agecheck();
 
-function toggleClass() {
-  this.classList.toggle('active');
-} function addClass() {
-  this.classList.add('finished');
-}
+document.querySelector('.player-button').disabled = true;
+document.querySelector(".player-Score").innerText = (`${playerName} score  = ${playerScore}`);
+document.querySelector(".computer-Score").innerHTML = (`Computer Score = ${computerScore}`);
+document.getElementById("demo").innerHTML = `${playerName} gooien`;
+document.getElementById("demo1").innerHTML = playerName;
+// document.querySelector('.playerCredits').innerHTML = `${playerName} credits zijn ${playerCredit}`
 
-function startGame() {
-if (computerBtn) {
-  computerBtn.addEventListener('click', function (click) {
-    let audio = new Audio('media/mouseclick2.mp3');
-    audio.play();
-    computerDice = Math.floor(Math.random() * 6);
-    computerDice++;
-    let computerDiceImg = 'images/dice' + computerDice + '.png'
-    document.querySelectorAll ('img')[0].setAttribute('src', computerDiceImg);
-    console.log(`Computer gooit ${computerDice}`);
-    document.getElementById("computer").innerHTML = computerDice;
-    document.querySelector('.computer-button').disabled = true;
-    document.querySelector('.player-button').disabled = false;
-  })};
 
 if (playerBtn) {
-playerBtn.addEventListener("click", function (click) {
+  playerBtn.addEventListener('click', playerButtonAction);
+}
+
+if (computerBtn) {
+  computerBtn.addEventListener('click', computerButtonAction);
+}
+
+function Agecheck () {
+
+  if (ageRestriction > 0 && ageRestriction < 18) {
+    alert(`Je bent onder de 18. Jij bent ${ageRestriction} jaar oud.`);
+    window.location.href = "tooYoung.html";
+  } else if (ageRestriction > "a" && ageRestriction < "z") {
+    window.location.href = "fout.html";
+    alert('Er is een fout opgetreden. Type alleen nummers (bijv. 18).');
+  } else if (ageRestriction > "A" && ageRestriction < "Z") {
+    alert('Er is een fout opgetreden. Type alleen nummers (bijv. 18).');
+    window.location.href = "fout.html";
+  } else {
+    alert(`Je bent ${ageRestriction} jaar oud.`);
+  }
+}
+
+function computerButtonAction() {
+
   let audio = new Audio('media/mouseclick2.mp3');
   audio.play();
-  playerDice = Math.floor(Math.random() * 6);
-  playerDice++;
-  let playerDiceImg = 'images/dice' + playerDice + '.png'
-  document.querySelectorAll ('img')[1].setAttribute('src', playerDiceImg);
+  computerRandomDice1 = Math.floor(Math.random() * 6);
+  computerRandomDice1++;
+  computerRandomDice2 = Math.floor(Math.random() * 6);
+  computerRandomDice2++;
+  computerDice = computerRandomDice1 + computerRandomDice2
+  
+  let computerDiceImg1 = 'images/dice' + computerRandomDice1 + '.png'
+  document.querySelectorAll ('img')[0].setAttribute('src', computerDiceImg1);
+
+  let computerDiceImg2 = 'images/dice' + computerRandomDice2 + '.png'
+  document.querySelectorAll ('img')[1].setAttribute('src', computerDiceImg2);
+
+  console.log(`Computer gooit ${computerDice}`);
+  document.getElementById("computer").innerHTML = computerDice;
+  document.querySelector('.computer-button').disabled = true;
+  document.querySelector('.player-button').disabled = false;
+}
+
+function playerButtonAction() {
+  
+  let audio = new Audio('media/mouseclick2.mp3');
+  audio.play();
+  playerRandomDice1 = Math.floor(Math.random() * 6);
+  playerRandomDice1++;
+  playerRandomDice2 = Math.floor(Math.random() * 6);
+  playerRandomDice2++;
+  playerDice = playerRandomDice1 + playerRandomDice2
+
+  let playerDiceImg1 = 'images/dice' + playerRandomDice1 + '.png'
+  document.querySelectorAll ('img')[2].setAttribute('src', playerDiceImg1);
+  
+  let playerDiceImg2 = 'images/dice' + playerRandomDice2 + '.png'
+  document.querySelectorAll ('img')[3].setAttribute('src', playerDiceImg2);
+
   console.log(`Speler gooit ${playerDice}`);
   document.getElementById("player").innerHTML = playerDice;
-  document.querySelector('.computer-button').disabled = false;
   document.querySelector('.player-button').disabled = true;
-  checkWinner();
-})};
-
-function checkWinner(){
- if (playerDice > computerDice) {
-  document.getElementById("demo2").innerText = (`${playerName} gooit hoger. ${playerName} gooit ${playerDice - computerDice} meer hoger dan de computer.`)
-  console.log(`${playerName} gooit hoger. ${playerName} gooit ${playerDice}.`);
-  playerScore = playerScore + 1;
-  document.querySelector(".player-Score").innerText = (`${playerName} score  = ${playerScore}`);
+  document.querySelector('.computer-button').disabled = false;
   
-  if (playerScore >= y){
-    playerScore = playerScore - y;
+  if (higher.checked) {
+    checkHigher()
+
+  } 
+  
+  else if (lower.checked) {
+    checkLower()
+
+  } else {
+    document.querySelector('.displayGame').innerHTML = "Kies higher of lower!!"
+  }
+
+  scoreReset()
+}
+
+function checkHigher() {
+  
+    lower.checked = false;
+
+    if (computerDice < playerDice) {
+      console.log('Je hebt Gewonnen')
+      playerScore = playerScore + 1;
+      document.querySelector(".displayGame").innerHTML = (`${playerName} heeft gewonnen.`)
+      document.querySelector(".player-Score").innerText = (`${playerName} score  = ${playerScore}`);
+      
+    } else if (playerDice < computerDice) {
+      console.log('Je hebt verloren!') 
+      computerScore = computerScore + 1;
+      document.querySelector(".displayGame").innerHTML = (`De computer heeft gewonnen.`)
+      document.querySelector(".computer-Score").innerHTML = (`Computer Score = ${computerScore}`);
+      
+    } else {
+      console.log('Draw')
+      document.querySelector(".displayGame").innerHTML = (`Draw`)
+  }
+}
+
+function checkLower() {
+
+  higher.checked = false;
+
+  if (computerDice > playerDice) {
+      console.log('Je hebt gewonnen')
+      playerScore = playerScore + 1;
+      document.querySelector(".player-Score").innerText = (`${playerName} score  = ${playerScore}`);
+      document.querySelector(".displayGame").innerHTML = (`${playerName} heeft gewonnen.`)
+
+    } else if (playerDice > computerDice) {
+      console.log('Je hebt verloren')
+      computerScore = computerScore + 1;
+      document.querySelector(".displayGame").innerHTML = (`De computer heeft gewonnen.`)
+      document.querySelector(".computer-Score").innerHTML = (`Computer Score = ${computerScore}`);
+
+    } else {
+      console.log('Draw')
+      document.querySelector(".displayGame").innerHTML = (`Draw`)
+  }
+}
+
+function scoreReset() {
+
+  if (playerScore == y) {
     const audio = new Audio('media/victory.mp3');
     audio.play();
-    console.log(playerScore)
+    playerScore = playerScore - y;
+    playerCredit++;
+    console.log(playerCredit)
+    document.querySelector('.playerCredits').innerHTML = `${playerName} credits zijn ${playerCredit}`
     document.querySelector(".player-Score").innerText = (`${playerName} score  = ${playerScore}`);
     computerScore = 0;
     document.querySelector(".computer-Score").innerText = (`Computer Score = ${computerScore}`);
@@ -96,49 +172,26 @@ function checkWinner(){
     playerWins++;
     document.querySelector(".player-Wins").innerText = (`${playerName} wins : ${playerWins}`);
   }
-  
- } else if (computerDice > playerDice) {
-  document.getElementById("demo2").innerText = (`De computer gooit hoger. Computer gooit ${computerDice - playerDice} meer hoger dan ${playerName}.`)
-  console.log(`De computer gooit hoger. De computer gooit ${computerDice}. ${playerName} heeft ${playerDice} gegooit.`);
-  computerScore = computerScore +  1;
-  document.querySelector(".computer-Score").innerText = (`Computer Score = ${computerScore}`);
-  
- 
-  if (computerScore >= y){
+
+  if (computerScore == y) {
     const audio = new Audio('media/victory.mp3');
     audio.play();
     computerScore = computerScore - y;
-    console.log(computerScore)
+    playerCredit = playerCredit - 2;
+    document.querySelector('.playerCredits').innerHTML = `${playerName} credits zijn ${playerCredit}`
     document.querySelector(".computer-Score").innerText = (`Computer Score = ${computerScore}`)
     playerScore = 0;
     document.querySelector(".player-Score").innerText = (`${playerName} score  = ${playerScore}`);
     alert(`Computer heeft gewonnen!!!. Scores worden gereset.`)
     computerWins++;
     document.querySelector(".computer-Wins").innerText = (`Computer wins : ${computerWins}`)
-    playerCredit = playerCredit - 1; 
-    console.log(`credit is ${playerCredit}`)
-   };
-
-} else{
-  document.getElementById("demo2").innerText = ("Draw")
-  console.log("Draw");
-}}};
-
-let higher = document.getElementById('Higher') 
-if (higher) {
-  higher.addEventListener('click', function (click) {
-  higher = true;
-  console.log(higher)
-  while (higher == true){
-  if (playerDice > computerDice){
-    console.log('Je hebt gewonnen')
-  }else {
-    console.log('Je bent verloren')
-    
   }
-  return higher;
 }
-  })};
+
+
+  
+
+
 
 
 
